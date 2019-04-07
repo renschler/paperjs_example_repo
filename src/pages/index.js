b38440acc3b0af964ec889d28e02844c157db99b
Patrick Renschler <patrick.renschler@gmail.com>
2019-04-07 13:15:30 -0500

    draw line

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default class index extends React.Component {
   componentDidMount() {
     let canvas = document.getElementById("myCanvas")
     paper.setup(canvas)
-    let line = new paper.Line({ x: 10, y: 10 }, { x: 40, y: 40 })
+    let line = new paper.Path.Line({ x: 10, y: 10 }, { x: 40, y: 40 })
     line.style = {
       strokeWidth: 1,
       strokeColor: "black",
