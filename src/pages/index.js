220ab66b24a8ddf60981ea6c0de3785af8a40963
Patrick Renschler <patrick.renschler@gmail.com>
2019-04-07 13:14:07 -0500

    add paperjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import paper from "paper/dist/paper-core"
 
-export default () => <div>Hello world!</div>
+export default class index extends React.Component {
+  componentDidMount() {
+    let canvas = document.getElementById("myCanvas")
+    paper.setup(canvas)
+    let line = new paper.Line({ x: 10, y: 10 }, { x: 40, y: 40 })
+    line.style = {
+      strokeWidth: 1,
+      strokeColor: "black",
+    }
+  }
+  render() {
+    return (
+      <div>
+        <canvas id="myCanvas" data-paper-resize="true" />
+      </div>
+    )
+  }
+}
